6431ffab2c002ab764c3ee5de551713828f83cb1
leebhoon <48741112+leebhoon@users.noreply.github.com>
2019-03-20 11:35:41 +0900

    Update hello.py

--- git_tutorial/hello.py
+++ git_tutorial/hello.py
@@ -2,9 +2,8 @@
 //first: github modification
 //second: local repository modification
 
-
 print("hello world")
 print("tell your world")
 print("tell his world")
 print("tell her world")
-
+print("tell my world")
